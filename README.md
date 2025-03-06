fc692b4f26c0dd8b426c23cdc6b05010e91e860e
nguyencatbo <nguyencatbo123@gmail.com>
2025-03-06 09:40:44 +0700

    Nop Bai

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Bai2.2
+# Nguyen Cat Bo
+### MSV: 22810310305
+## Flatlist
+## Ket qua
+
+![anh1](anh1.jpg)
+
---
+++ my-app/App.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import { FlatList, View, Text, StyleSheet, Image } from 'react-native';
+
+const notifications = [
+  {
+    id: '1',
+    title: 'Bước 1 Xác định nhu cầu khách hàng',
+    description: 'Vũ Văn Hoàng sắp đến hạn lúc 01/08/2020 9:00',
+    time: '20/08/2020, 06:00',
+    icon: require('./favicon.png'), // Đường dẫn ảnh (tuỳ bạn thêm)
+  },
+  {
+    id: '2',
+    title: 'Bạn có khách hàng mới!',
+    description: 'Chúc mừng bạn, bạn có khách hàng mới. Hãy mau chóng liên lạc ngay.',
+    time: '20/08/2020, 06:00',
+    icon: require('./favicon.png'), // Đường dẫn ảnh khác
+  },
+  {
+    id: '3',
+    title: 'Khách hàng được chia sẻ bị trùng',
+    description: 'Rất tiếc, khách hàng được chia sẻ đã tồn tại trên hệ thống. Vui lòng chia sẻ khách hàng.',
+    time: '20/08/2020, 06:00',
+    icon: require('./favicon.png'),
+  },
+  {
+    id: '4',
+    title: 'Khách hàng được thêm bị trùng',
+    description: 'Rất tiếc, khách hàng được thêm đã tồn tại trên hệ thống. Vui lòng thêm khách hàng.',
+    time: '20/08/2020, 06:00',
+    icon: require('./favicon.png'),
+  },
+  {
+    id: '5',
+    title: 'Công việc sắp đến hạn trong hôm nay',
+    description: 'Bạn có 17 công việc sắp đến hạn trong hôm nay.',
+    time: '20/08/2020, 06:00',
+    icon: require('./favicon.png'),
+  },
+  {
+    id: '6',
+    title: 'Công việc đã quá hạn',
+    description: 'Bạn có 17 công việc bị quá hạn. Hãy kiểm tra và lên kế hoạch hoàn thành công việc.',
+    time: '20/08/2020, 06:00',
+    icon: require('./favicon.png'),
+  },
+];
+
+const App = () => {
+  const renderItem = ({ item }) => (
+    <View style={styles.itemContainer}>
+      <Image source={item.icon} style={styles.icon} />
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>{item.title}</Text>
+        <Text style={styles.description}>{item.description}</Text>
+        <Text style={styles.time}>{item.time}</Text>
+      </View>
+    </View>
+  );
+
+  return (
+    <FlatList
+      data={notifications}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.id}
+      contentContainerStyle={styles.list}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  list: {
+    paddingTop:100,
+    padding: 10,
+    backgroundColor: '#f5f5f5',
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 8,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 2, // Hiệu ứng đổ bóng trên Android
+  },
+  icon: {
+    width: 40,
+    height: 40,
+    marginRight: 10,
+  },
+  textContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  description: {
+    fontSize: 14,
+    color: '#666',
+    marginTop: 4,
+  },
+  time: {
+    fontSize: 12,
+    color: '#999',
+    marginTop: 4,
+  },
+});
+
+export default App;
